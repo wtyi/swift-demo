1e581a3a7f940df679969c3004ce1236c48a3e46
wty <89521515@qq.com>
2021-01-21 21:58:16 +0800

    Initial Commit

---
+++ apple-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apple-demo
+//
+//  Created by 王天怡 on 2021/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple-demo/apple_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_demoApp.swift
+//  apple-demo
+//
+//  Created by 王天怡 on 2021/1/21.
+//
+
+import SwiftUI
+
+@main
+struct apple_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
